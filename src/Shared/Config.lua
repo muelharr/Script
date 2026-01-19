deab93ef9f996872ff6490d226e1e6e62ad6aa33
Samuel Lumbantoruan <lumbantoruansamuel07@gmail.com>
2026-01-20 00:32:33 +0700

    Initial commit

---
+++ src/Shared/Config.lua
@@ -0,0 +1,9 @@
+return {
+    CAST_ANIM   = "rbxassetid://17876233325",
+    IDLE_ANIM   = "rbxassetid://17876234456",
+    MESH_ID     = "rbxassetid://12223378918",
+    TEX_ID      = "rbxassetid://12223379005",
+    GLOW_PART_ID= "rbxassetid://301049944",
+    CAST_SOUND  = "rbxassetid://9063097111",
+    HOTKEY      = Enum.KeyCode.F6,
+}
